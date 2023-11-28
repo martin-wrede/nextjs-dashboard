65a4bacbe47019d1f66ecda092dc10b61266cc72
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2023-11-28 16:10:52 +0100

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { lusitana } from 'next/font/google';
  
 export default function RootLayout({
   children,
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,13 @@
-import { Inter, Lusitana } from 'next/font/google';
+ import { Inter, Lusitana } from 'next/font/google';
 
+// import localFont from 'next/font/local';
+
+// export const inter = localFont({ src: './Inter-Medium.ttf'})
+
+
+
+ export const inter = Inter({ subsets: ['latin'] }); 
+
+ 
 export const lusitana = Lusitana({ weight:['400','700'], subsets: ['latin'] });
-export const inter = Inter({ subsets: ['latin'] }); 
+
